a745c256c9887613eda6594c3e7badbac10b906b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-22 12:48:27 +0000

    Deploy junusheva/image_slider to github.com/junusheva/image_slider.git:gh-pages

---
+++ precache-manifest.36de4af73ec512ebafff6ddb9b00c030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b680265c41b5ca9ac9f6",
+    "url": "/image_slider/static/css/main.3a8b3d03.chunk.css"
+  },
+  {
+    "revision": "b680265c41b5ca9ac9f6",
+    "url": "/image_slider/static/js/main.b680265c.chunk.js"
+  },
+  {
+    "revision": "1a8b8942d8143e7b72bb",
+    "url": "/image_slider/static/js/runtime~main.1a8b8942.js"
+  },
+  {
+    "revision": "fc64328732eeaa7cd20b",
+    "url": "/image_slider/static/css/2.226654d1.chunk.css"
+  },
+  {
+    "revision": "fc64328732eeaa7cd20b",
+    "url": "/image_slider/static/js/2.fc643287.chunk.js"
+  },
+  {
+    "revision": "4c5426b535d1c51fbbcc0540ec0c3e53",
+    "url": "/image_slider/index.html"
+  }
+];
